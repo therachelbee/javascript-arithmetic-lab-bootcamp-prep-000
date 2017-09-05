64f86731a9185bc17968d280e905d92e3a029a0d
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-05 00:21:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function add(a,b) { return a + b }
 function subtract(a,b) { return a - b }
 function multiply(a,b) { return a * b }
 function divide(a,b) { return a / b }
-function inc(n) { return n++ }
+function inc(n) { return n+1 }
