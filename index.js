bd85b248b0091f881c68545d9c11361c7842c24d
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-05 00:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function multiply(a,b) { return a * b }
 function divide(a,b) { return a / b }
 function inc(n) { return n+1 }
 function dec(n) { return n-1 }
-function parsInt(n, 10)
+function parsInt('n', 10)
